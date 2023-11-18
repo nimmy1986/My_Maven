3f0876c86e364c5464638adb07de2e3fc48bb32b
nimmy1986 <148308970+nimmy1986@users.noreply.github.com>
2023-11-18 11:41:44 +0530

    Final Keyword

---
+++ My_Maven_Project1/src/main/java/FinalKeywords/FinalChild.java
@@ -0,0 +1,14 @@
+package FinalKeywords;
+
+public final class FinalChild extends FinalParent
+{
+	public final void hello() 
+	{
+		System.out.println("Final keyword"+a);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Final world");
+	}
+
+}
---
+++ My_Maven_Project1/src/main/java/FinalKeywords/FinalParent.java
@@ -0,0 +1,20 @@
+package FinalKeywords;
+
+public class FinalParent 
+{
+	final int a=48;
+	public FinalParent()
+	{
+		int a=4;
+	}
+	
+	public void hello() 
+	{
+		System.out.println("Final keyword"+a);
+	}
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
